b5120fb23704033e148ca778d2df144d3eb6c18f
Dean Levinson <dean.e.levinson@gmail.com>
2017-04-09 18:34:29 +1000

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :subreddits
   resources :users
+  root "user_view#index"
   get "view/subreddits" => "user_view#subreddits"
   get "view/users" => "user_view#users"
   get "view/comments" => "user_view#comments"
